590a6dabaf3fd9aa9a3b56627efb0340292e5382
mogamoga <mogamoga.g1thu6@gmail.com>
2024-10-21 00:30:53 +0900

    好きだから言えないの

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const App = {
+    data() {
+        return {
+            // todo
+        };
+    },
+    created() {
+        // todo
+    },
+    methods: {
+        // todo
+    }
+};
+
+Vue.createApp(App).mount("#app");
